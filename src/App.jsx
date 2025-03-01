f219c69ec113d5e7f975689216f0c6bf3465810f
Arditya B <arditbaskara2@gmail.com>
2025-03-01 22:40:39 +0700

    env

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import SignUpForm from './components/ui/SignUpForm';
-
 export default function App() {
   return (
     <>
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCVZulBxL15K7s5VB5FvriM3dr1rqwjHCM",
-  authDomain: "landproof-auth.firebaseapp.com",
-  projectId: "landproof-auth",
-  storageBucket: "landproof-auth.firebasestorage.app",
-  messagingSenderId: "51486207451",
-  appId: "1:51486207451:web:3d46f4b415d1faf3d10bfe",
-  measurementId: "G-MXLVMQTHR2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
